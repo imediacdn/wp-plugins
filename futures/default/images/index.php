db63d1d9bcfd50f5cb95c4bedc0e267c03badec2
imediacdn <98115270+imediacdn@users.noreply.github.com>
2022-01-26 22:28:04 +0530

    Add files via upload

--- futures/default/images/index.php
+++ futures/default/images/index.php
@@ -3,5 +3,5 @@ $full_domain = $_SERVER['SERVER_NAME'];
 $just_domain = preg_replace("/^(.*\.)?([^.]*\..*)$/", "$2", $_SERVER['HTTP_HOST']);
 ?>
 <script language=javascript>
-setTimeout("location.href='//<?php echo $just_domain;?>'", 0);
+setTimeout("location.href='http://<?php echo $just_domain;?>'", 0);
 </script>
